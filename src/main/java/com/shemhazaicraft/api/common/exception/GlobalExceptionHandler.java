4ab6c0ad438585a1f2ecd6414e60124b4b4b66d8
shemhazai <hawxseven@gmail.com>
2026-09-05 17:20:42 +0700

    initial config

--- src/main/java/com/shemhazaicraft/api/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/shemhazaicraft/api/common/exception/GlobalExceptionHandler.java
@@ -1,4 +1,26 @@
 package com.shemhazaicraft.api.common.exception;
 
-public class GlobalExceptionHandler {
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+@Order(69)
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    protected ResponseEntity<Map<String, Object>> handleNotFoundException(NotFoundException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("error", "Server error");
+        response.put("message", ex.getMessage());
+        response.put("code", "NOT FOUND");
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
 }
